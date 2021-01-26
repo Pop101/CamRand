2c738af06bce3e71a7f46dafe1bb14da6181e671
tennisbowling <enzovanthorre@hotmail.com>
2021-01-26 00:17:20 -0800

    test travis

---
+++ tests/travis.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get(f"https://rand.tennisbowling.com/random")
+print(response)
+if response == "<Response [200}>":
+    exit(0)
+else:
+    sys.exit("Api response is not 200! Please check the api.")
