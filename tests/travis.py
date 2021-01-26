85cd8df80189ce2277b83718f9a6155c04d82cdb
tennisbowling <enzovanthorre@hotmail.com>
2021-01-26 08:56:55 -0800

    dfghg

--- tests/travis.py
+++ tests/travis.py
@@ -4,12 +4,11 @@ import sys
 
 the_data = dict()
 
-### return 0 if problem, else return the json payload
 url = "https://rand.tennisbowling.com/random"
 load = requests.get (url).json()
-if load['status'] != 'OK':
-    print("api is working as intended")
+if load['status'] == 'OK':
+    print("Api is working as intended.")
 else:
-    sys.exit("Api did not return 200 ok. please check api.")
+    sys.exit("Api is down or not working. please check it.")
 
 
