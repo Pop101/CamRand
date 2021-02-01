e796132ebccf622cea209e0be12a472af10dd3aa
Pop101 <leon.leibmann@gmail.com>
2021-01-31 21:26:45 -0800

    Forgot to cast

--- camrand.py
+++ camrand.py
@@ -37,8 +37,8 @@ class RandomImageSource:
             for y in range(img.height):
                 rand += algorith(px[x, y])
         
-        self.last_random = rand
-        return rand
+        self.last_random = int(rand, 2)
+        return int(rand, 2)
     
     def get_seed(self):
         rand_int = self.last_random - self.get_raw_int(algorith=self.gray_algo)
